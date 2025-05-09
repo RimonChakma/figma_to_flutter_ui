04d337fc8f6ecfe16f1778b7e89f7ffea6e5b8c5
RimonChakma <rimon.chakma880@gmail.com>
2025-05-09 21:53:42 +0600

    create wish list structure

--- lib/main.dart
+++ lib/main.dart
@@ -24,82 +24,13 @@ class cetagorieScreen extends StatefulWidget {
 
 class _cetagorieScreenState extends State<cetagorieScreen> {
 
-  List<Map<String, String>> imageFile = [
-    {"path": "assets/homeScreen/laptop.png", "name": "electronic"},
-    {"path": "assets/homeScreen/fruits.png", "name": "Fruits"},
-    {"path": "assets/homeScreen/diamond.png", "name": "fashion"},
-    {"path": "assets/homeScreen/car.png", "name": "furniture"},
-    {"path": "assets/homeScreen/laptop.png", "name": "electronic"},
-    {"path": "assets/homeScreen/fruits.png", "name": "Fruits"},
-    {"path": "assets/homeScreen/diamond.png", "name": "fashion"},
-    {"path": "assets/homeScreen/car.png", "name": "furniture"},
-    {"path": "assets/homeScreen/laptop.png", "name": "electronic"},
-    {"path": "assets/homeScreen/fruits.png", "name": "Fruits"},
-    {"path": "assets/homeScreen/diamond.png", "name": "fashion"},
-    {"path": "assets/homeScreen/car.png", "name": "furniture"},
-    {"path": "assets/homeScreen/laptop.png", "name": "electronic"},
-    {"path": "assets/homeScreen/fruits.png", "name": "Fruits"},
-    {"path": "assets/homeScreen/diamond.png", "name": "fashion"},
-    {"path": "assets/homeScreen/car.png", "name": "furniture"},
-  ];
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("categories"),
+        title: Text("Wish List"),
       ),
-      body: GridView.builder(
-        itemCount: imageFile.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-
-      ), itemBuilder: (context, index) {
-          return
-            Column(
-            children: [
-              Center(
-                child: SizedBox(
-                  height: 90,
-                  width: 90,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.2),
-                    ),
-                    child: Center(
-                      child: Image.asset(
-                        imageFile[index]["path"]!,
-                        fit: BoxFit.contain,
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-
-              SizedBox(height: 6,),
-              Text(imageFile[index]["name"]!)
-
-            ],
-          );
-      },),
-      bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.grey,
-          unselectedItemColor: Colors.grey,
-          type: BottomNavigationBarType.fixed,
-
-          currentIndex: 0,
-          items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
-        BottomNavigationBarItem(icon: Icon(Icons.widgets_sharp),label: "categories"),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),label: "gift"),
-        BottomNavigationBarItem(icon: Icon(Icons.card_giftcard),label: "wish"),
-      ]),
     );
   }
 }
