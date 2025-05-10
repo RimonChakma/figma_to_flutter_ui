a41d9d78162a745a32a0fa9d5e464228f5cb80d0
RimonChakma <rimon.chakma880@gmail.com>
2025-05-10 17:56:07 +0600

    add product details

--- lib/main.dart
+++ lib/main.dart
@@ -55,15 +55,58 @@ class _cetagorieScreenState extends State<cetagorieScreen> {
           ),
           itemBuilder: (context, index) {
             return Card(
-              color: Colors.grey,
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Image.asset(
-                    imageFile[index],
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.contain,
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Image.asset(
+                      imageFile[index],
+                      height: 80,
+                      width: 80,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Special Shoes 30%",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("\$100", style: TextStyle(fontSize: 12)),
+                              Row(
+                                children: [
+                                  Icon(Icons.star, size: 14, color: Colors.orange),
+                                  Text("4.8", style: TextStyle(fontSize: 12)),
+                                ],
+                              ),
+                              Icon(Icons.favorite_border, size: 16),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
