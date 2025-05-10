e267bd98e6bdcdfb82fabb29b7205e061124a547
RimonChakma <rimon.chakma880@gmail.com>
2025-05-10 17:28:26 +0600

    use GridViewBuilder and add image

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,59 @@ class cetagorieScreen extends StatefulWidget {
 class _cetagorieScreenState extends State<cetagorieScreen> {
 
 
+  List imageFile = [
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+    "assets/homeScreen/shoes.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Wish List"),
       ),
+
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: imageFile.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.grey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    imageFile[index],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.contain,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+
+
+
       bottomNavigationBar: BottomNavigationBar(
           selectedItemColor: Colors.grey,
           unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
           currentIndex: 0,
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home),label:"home",),
