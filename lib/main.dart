ff3721f0bfb66475fd82807c0462df8022c496dc
RimonChakma <rimon.chakma880@gmail.com>
2025-05-30 23:14:29 +0600

    add TextField

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,50 @@ class CreateReviewForm extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("products details")),
 
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey.shade500,
+                width: 1
+              )
+            ),
+            child: TextField(decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              hintText: "First Name",
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            ),),
+          ),
+        ),
+
+        SizedBox(height: 10,),
+
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.grey.shade500,
+                    width: 1
+                )
+            ),
+            child: TextField(decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              hintText: "Last Name",
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            ),),
+          ),
+        ),
+      ],),
+
       bottomNavigationBar: BottomNavigationBar(
           selectedItemColor: Colors.grey,
           unselectedItemColor: Colors.grey,
